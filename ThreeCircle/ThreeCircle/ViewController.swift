d4537b513441d78cd816d18658e0ca158dabdad7
杨辉 <453413516@qq.com>
2016-07-10 20:25:36 +0800

    修复定时起问题，现在已经能够完美运行

--- ThreeCircle/ThreeCircle/ViewController.swift
+++ ThreeCircle/ThreeCircle/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //左边的圆形
     var leftCirCle:UIView?;
+    //中间的圆形
     var centerCirCle:UIView?;
+    //右边的圆形
     var rightCirCle:UIView?
+    //定时器
     var timer:Timer? = nil;
+    //圆形半径
     let Radius = 30;
     
     static var playCount = 0;
@@ -31,16 +36,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /**
+     * 设置定时起
+     * 
+     * @return Timer? 返回一个创建好的定时起
+     */
     func setTimer() -> Timer?{
         if self.timer == nil {
-            self.timer = Timer(timeInterval: 3.0, target: self, selector: #selector(firstAnimation(_:)), userInfo: nil, repeats: true);
+            //swift中使用这个创建定时起，可以实现重复的功能
+            self.timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(firstAnimation(_:)), userInfo: nil, repeats: true);
+            //下面注释掉的语句无法实现重复的功能，只能够实现单次激活
+//            self.timer = Timer(timeInterval: 3.0, target: self, selector: #selector(firstAnimation(_:)), userInfo: nil, repeats: true);
             self.timer?.fire();
         }
         
         return self.timer;
     }
     
+    /**
+     * 增加三个圆形
+     */
     func addThreeCir(){
+        //创建中间的圆形
         let centerCir = UIView(frame: CGRect(x: 0, y: 0, width: Radius, height: Radius));
         centerCir.center = self.view.center;
         centerCir.layer.cornerRadius = CGFloat(Radius) * CGFloat(0.5);
@@ -78,23 +95,23 @@ class ViewController: UIViewController {
         ViewController.playCount += 1;
         print(ViewController.playCount);
         
-//        UIView.animate(withDuration: 1.0, animations: {
-//            
-//            self.leftCirCle?.transform = CGAffineTransform(translationX: -(CGFloat)(self.Radius), y: 0);
-//            self.leftCirCle?.transform = CGAffineTransform(scaleX: 0.75, y: 0.75);
-//            self.rightCirCle?.transform = CGAffineTransform(translationX: CGFloat(self.Radius), y: 0);
-//            self.rightCirCle?.transform = CGAffineTransform(scaleX: 0.75, y: 0.75);
-//            self.centerCirCle?.transform = CGAffineTransform(scaleX: 0.75, y: 0.75);
-//            
-//            }, completion: {
-//                finished in
-//                UIView.animate(withDuration: 1.0, animations: {
-//                    self.leftCirCle?.transform = CGAffineTransform.identity;
-//                    self.rightCirCle?.transform = CGAffineTransform.identity;
-//                    self.centerCirCle?.transform = CGAffineTransform.identity;
-//                    self.secondAnimaition();
-//                })
-//        })
+        UIView.animate(withDuration: 1.0, animations: {
+            
+            self.leftCirCle?.transform = CGAffineTransform(translationX: -(CGFloat)(self.Radius), y: 0);
+            self.leftCirCle?.transform = CGAffineTransform(scaleX: 0.75, y: 0.75);
+            self.rightCirCle?.transform = CGAffineTransform(translationX: CGFloat(self.Radius), y: 0);
+            self.rightCirCle?.transform = CGAffineTransform(scaleX: 0.75, y: 0.75);
+            self.centerCirCle?.transform = CGAffineTransform(scaleX: 0.75, y: 0.75);
+            
+            }, completion: {
+                finished in
+                UIView.animate(withDuration: 1.0, animations: {
+                    self.leftCirCle?.transform = CGAffineTransform.identity;
+                    self.rightCirCle?.transform = CGAffineTransform.identity;
+                    self.centerCirCle?.transform = CGAffineTransform.identity;
+                    self.secondAnimaition();
+                })
+        })
     }
     
     func secondAnimaition(){
